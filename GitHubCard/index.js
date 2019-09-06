edec81d19bbad81a56d0b678feac20ccc54ce141
abarne <celius377@gmail.com>
2019-09-06 09:22:33 -0500

    fixed name displaying

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -49,6 +49,61 @@ var followersArray = [];
 
 let cardEntry = document.querySelector('.cards');
 
+var proxy = 'https://urlreq.appspot.com/req?method=GET&url=',
+	url = proxy + 'https://github.com/abarne',
+	colors = {
+		eeeeee: 'github-graph-none',
+		d6e685: 'github-graph-litte',
+		'8cc665': 'github-graph-some',
+		'44a340': 'github-graph-more',
+		'1e6823': 'github-graph-most'
+	};
+
+document.addEventListener('DOMContentLoaded', function() {
+	fetch(url)
+		.then(function responseThen(response) {
+			return response.text();
+		})
+		.then(function fetchThen(body) {
+			var wrapper = document.createElement('div'),
+				container = document.querySelector('.graph'),
+				graph,
+				gs,
+				fill,
+				index;
+
+			wrapper.innerHTML = body;
+
+			graph = wrapper.querySelector('svg.js-calendar-graph-svg');
+			gs = [].slice.call(graph.querySelectorAll('g g'));
+
+			for (var g of gs) {
+				index = gs.indexOf(g);
+
+				if (index < 26) {
+					g.parentElement.removeChild(g);
+				} else {
+					g.attributes.transform.value = 'translate(' + 13 * (index - 26) + ', 0)';
+				}
+			}
+
+			for (var rect of graph.querySelectorAll('rect')) {
+				fill = rect.getAttribute('fill').substring(1);
+
+				if (colors[fill]) {
+					rect.classList.add(colors[fill]);
+				}
+			}
+
+			graph.setAttribute('version', '1.1');
+			graph.setAttribute('xmlns', 'http://www.w3.org/2000/svg');
+			graph.setAttribute('xmlns:xlink', 'http://www.w3.org/1999/xlink');
+			graph.setAttribute('xml:space', 'preserve');
+
+			container.innerHTML = graph.outerHTML;
+		});
+});
+
 function gitCard(obj) {
 	//create componenets
 	let card = document.createElement('div'),
@@ -71,6 +126,7 @@ function gitCard(obj) {
 	//	append children
 	card.appendChild(newImg);
 	card.appendChild(newCardInfo);
+	newCardInfo.appendChild(newHeader);
 	newCardInfo.appendChild(pUserName);
 	newCardInfo.appendChild(pLocation);
 	newCardInfo.appendChild(pProfile);
@@ -81,7 +137,7 @@ function gitCard(obj) {
 
 	//add content
 	newImg.src = obj.data.avatar_url;
-	newHeader = `Name: ${obj.data.name}`;
+	newHeader.textContent = `Name: ${obj.data.name}`;
 	pUserName.textContent = `Username: ${obj.data.login}`;
 	pLocation.textContent = `Location: ${obj.data.location}`;
 	pProfile.innerHTML = `<a href=${obj.data.html_url}> Profile </a>`;
@@ -113,7 +169,7 @@ axios
 		console.log(response);
 
 		//go through each follower and create their cards
-		response.data.map((item) => {
+		response.data.slice(0, 9).map((item) => {
 			axios
 				.get(`https://api.github.com/users/${item.login}`)
 				.then((response) => {
